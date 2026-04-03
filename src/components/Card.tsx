c35e45bc9db8f06154431ae3afa5bbbca40610e2
PerthTheIamCat <thongcum2546@gmail.com>
2026-04-03 20:20:12 +0700

    feat: add tailwind-merge for dynamic class handling in Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,31 @@
 import type { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 export default function Card({
   title,
   description,
+  isAnimated = true,
   children,
+  className,
 }: Readonly<{
-  title: string;
-  description: string;
+  title?: string;
+  description?: string;
+  isAnimated?: boolean;
   children: ReactNode;
+  className?: string;
 }>) {
+  const thisClassName = twMerge(
+    `w-full max-w-md rounded-2xl border border-white/10 p-6 backdrop-blur-sm bg-white/5 ${
+      isAnimated &&
+      "transition-all ease-spring-elastic duration-950 active:scale-95 hover:shadow-2xl hover:shadow-black/30 hover:backdrop-blur-lg hover:bg-white/10 hover:scale-105 "
+    }`,
+    className,
+  );
+
   return (
-    <div className="w-full max-w-md rounded-2xl border border-white/10 p-6 backdrop-blur-sm hover:backdrop-blur-lg bg-white/5 hover:bg-white/10 hover:scale-105 transition-all ease-spring-elastic duration-950 hover:shadow-2xl hover:shadow-black/30 active:scale-95">
-      <h2 className="mb-2 text-lg font-semibold">{title}</h2>
-      <p className="mb-4 text-sm">{description}</p>
+    <div className={thisClassName}>
+      {title && <h2 className="mb-2 text-lg font-semibold">{title}</h2>}
+      {description && <p className="mb-4 text-sm">{description}</p>}
       {children}
     </div>
   );
